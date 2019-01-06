e1bbd3fc6181b338364200832958a899c586631f
trabdlkarim <trabdlkarim@programmer.net>
2019-01-06 21:45:51 +0300

    make main function more readable

--- helloworld/main.c
+++ helloworld/main.c
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.c
  * Author: trabdlkarim
@@ -14,7 +8,7 @@
 #include <stdlib.h>
 
 /*
- * 
+ *  Simple Hello World Program in C
  */
 int main(int argc, char** argv) {
     

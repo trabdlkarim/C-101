30aed8a61e0e732b0c51c0ae51746997ddec0372
trabdlkarim <trabdlkarim@programmer.net>
2019-01-29 21:45:51 +0300

    update main file to be more readable

--- datatypes/main.c
+++ datatypes/main.c
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.c
  * Author: trabdlkarim
@@ -15,7 +9,7 @@
 #include <float.h>
 
 /*
- * 
+ *  C Data Types Program
  */
 
 int main(int argc, char** argv) {

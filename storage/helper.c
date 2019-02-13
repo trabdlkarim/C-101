d9d53af04fafe032830d659a1c18cd8b97cb9215
trabdlkarim <trabdlkarim@programmer.net>
2019-02-13 21:45:51 +0300

    fix ld error 1: undefined reference to 'extern_storage_class'

--- storage/helper.c
+++ storage/helper.c
@@ -9,6 +9,7 @@
 // global variable having its declaration in main.c
 extern int count2;
  
-void extern_static_class(void) {
-   printf("count is %d\n", count2);
+void extern_storage_class(void) {
+    
+   printf("exten count is equal to %d\n", count2);
 }

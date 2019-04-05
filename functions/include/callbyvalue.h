25a7f8ab36ffe63005bcb53fb6d7f6c024afa28b
trabdlkarim <trabdlkarim@programmer.net>
2019-04-05 23:31:44 +0300

    add headers

---
+++ functions/include/callbyvalue.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   callbyvalue.h
+ * Author: trabdlkarim
+ *
+ */
+
+#ifndef CALLBYVALUE_H
+#define CALLBYVALUE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void roll_six_sided_dice(int n);
+int generate_rand_num(int limit);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CALLBYVALUE_H */
+

fa4acc6955110077647e3925ab82606431346042
trabdlkarim <trabdlkarim@programmer.net>
2019-04-05 23:31:44 +0300

    add main func

---
+++ functions/main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: trabdlkarim
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "include/callbyvalue.h"
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    roll_six_sided_dice(20);
+    
+    return (EXIT_SUCCESS);
+}
+

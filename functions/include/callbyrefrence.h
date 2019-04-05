2cb594f77615e05d723034751173c92d8b8185d6
trabdlkarim <trabdlkarim@programmer.net>
2019-04-05 23:31:44 +0300

    add headers

---
+++ functions/include/callbyrefrence.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   callbyrefrence.h
+ * Author: trabdlkarim
+ *
+ */
+
+#ifndef CALLBYREFRENCE_H
+#define CALLBYREFRENCE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CALLBYREFRENCE_H */
+

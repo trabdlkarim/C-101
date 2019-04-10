e02795870094dd0a2228cc8603b2000056e6cbde
trabdlkarim <trabdlkarim@programmer.net>
2019-04-10 23:31:44 +0300

    update main function

--- functions/main.c
+++ functions/main.c
@@ -15,12 +15,15 @@
  */
 int main(int argc, char** argv) {
     
-    //roll_six_sided_dice(50);
+    int n;
     
+    //roll_six_sided_dice(50);
     //printf("\n-----------------------Craps game party-------------------\n");
     //craps_dice_game();
-    printf("facotorial 5: %d",factorial_rec(5));
     
+    printf("facotorial (loop) of 5: %d\n",factorial_loop(5));
+    printf("facotorial (recursion) of 5: %d\n",factorial_rec(5));
+    printf("facotorial (tail) of 5: %d\n",factorial_tail(5));
     return (EXIT_SUCCESS);
 }
 

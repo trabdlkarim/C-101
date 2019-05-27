207e8a929ca541823d768f7483832fe9f817889a
trabdlkarim <trabdlkarim@programmer.net>
2019-05-27 23:23:55 +0300

    add inc header

---
+++ arrays/include/arrays.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   arrays.h
+ * Author: trabdlkarim
+ */
+
+#ifndef ARRAYS_H
+#define ARRAYS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    int gen_rand_num(int a, int b);
+    void init_array(int size, int array[]);
+    void print_array(int size, int array[]);
+    void frequency(int size, int array[]);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ARRAYS_H */
+
